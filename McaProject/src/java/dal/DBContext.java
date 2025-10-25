ed7b5cb03a5ab5f0fa89e846975dec1e57ccef00
friedrich <wquangu@gmail.com>
2025-10-25 11:49:45 +0700

    Refactor DBContext to abstract class
    - Converted DBContext to an abstract class with generic type parameter <T extends BaseObject>.
    - Added abstract method declarations: list(), get(int id), insert(T model), update(T model), delete(T model).

--- McaProject/src/java/dal/DBContext.java
+++ McaProject/src/java/dal/DBContext.java
@@ -1,10 +1,12 @@
 package dal;
 
+import model.BaseObject;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class DBContext {
+public abstract class DBContext<T extends BaseObject>{
     protected Connection connection;
 
     public DBContext() {
@@ -14,13 +16,33 @@ public class DBContext {
             String pass = "sa";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
-            System.out.println("✅ Kết nối DB thành công");
+            System.out.println("Kết nối DB thành công");
         } catch (ClassNotFoundException e) {
-            System.out.println("❌ Không tìm thấy driver JDBC");
+            System.out.println("Không tìm thấy driver JDBC");
             e.printStackTrace();
         } catch (SQLException e) {
-            System.out.println("❌ Kết nối DB lỗi: " + e.getMessage());
+            System.out.println("Kết nối DB lỗi: " + e.getMessage());
             e.printStackTrace();
         }
     }
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Đóng kết nối DB thành công!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Lỗi khi đóng kết nối DB: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+public abstract ArrayList<T> list();
+    public abstract T get(int id);
+    public abstract void insert(T model);
+    public abstract void update(T model);
+    public abstract void delete(T model);
 }
